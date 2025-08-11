de0175d293e286ba96ad6c0fccf3c3d02a8b8be5
BhaktiLead <bhaktileader@gmail.com>
2025-08-11 08:41:03 +0530

    Update cnn_model.py
    Added = > from tensorflow.keras.layers import BatchNormalization
    As Version control

--- scripts/cnn_model.py
+++ scripts/cnn_model.py
@@ -4,7 +4,8 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout, Flatten,\
  Conv2D, MaxPooling2D
-from keras.layers.normalization import BatchNormalization
+#from keras.layers.normalization import BatchNormalization 
+from tensorflow.keras.layers import BatchNormalization
 import numpy as np
 import directory_structure
 import os
